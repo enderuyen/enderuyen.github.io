103ef68a7bee6be0ca41abae1acae2a17c5dfca7
enderuyen <enderuyen@gmail.com>
2021-08-08 21:13:08 +0700

    huihi

--- lothimay/js/script.js
+++
@@ -1,9 +0,0 @@
-function play() {
-    if (document.getElementById("audio").play()) {
-        document.getElementById("video").pause();
-    }
-    if (document.getElementById("video").play()) {
-        document.getElementById("audio").pause();
-    }
-    document.getElementById("hihi").innerHTML = "Xin chào";
-}
